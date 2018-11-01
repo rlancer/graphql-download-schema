4ffa32fef614d6b39f7fe3605cf3fb8e855f70e1
Robert Lancer <rob@collaborizm.com>
2018-10-31 23:00:16 -0400

    updated cli

--- src/downloadSchema.ts
+++ src/downloadSchema.ts
@@ -1,27 +1,22 @@
-import { introspectionQuery, buildClientSchema, printSchema, parse } from 'graphql'
-import axios from 'axios'
+import * as fs from "fs";
+import {
+  introspectionQuery,
+  buildClientSchema,
+  printSchema,
+  parse
+} from "graphql";
+import axios from "axios";
 
-const endpoint process.argv[process.argv.length -2]
-const outfile = 'schema.graphql'] = process.argv
+async function downloadSchema(endpoint: string, outfile: string) {
+  const postBody = {
+    query: parse(introspectionQuery),
+    operationName: "IntrospectionQuery"
+  };
 
-console.log(process.argv)
+  const data = (await axios.post(endpoint, postBody)).data.data;
 
-if (!endpoint) {
-  console.log('Missing path to your schema')
-  console.log('Useage')
-  console.log('$> download-graphql-schema http://path_to_your_schema out_file.schema')
+  const schema = printSchema(buildClientSchema(data));
+  fs.writeFileSync(outfile, schema);
 }
 
-console.log(`Downloading schema from ${endpoint}`)
-const postBody = {
-  query: parse(introspectionQuery),
-  operationName: 'IntrospectionQuery'
-}
-;(async () => {
-  const data = (await axios.post(endpoint, postBody)).data.data
-
-  const schema = printSchema(buildClientSchema(data))
-  fs.writeFileSync(outfile, schema)
-
-  console.log(`Wrote schema to ${outfile}!`)
-})()
+export { downloadSchema };
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { Command, flags } from "@oclif/command";
+import { downloadSchema } from "./downloadSchema";
 
 class Mynewcli extends Command {
-  static description = "describe the command here";
+  static description =
+    "Download a GraphQL schema by providing an endpoint and an optional out file.";
 
   static flags = {
     // add --version flag to show CLI version
@@ -16,14 +18,23 @@ class Mynewcli extends Command {
   };
 
   static args = [
-    { name: "endpoint", description: "Path to your GraphQL endpoint" }
+    {
+      name: "endpoint",
+      description:
+        "URI to your GraphQL endpoint, ex: https://api.example.com/graphql "
+    }
   ];
 
   async run() {
     const { args, flags } = this.parse(Mynewcli);
 
-    // const name = flags.name || "world";
-    this.log(`hello ${args} + ${flags} from ./src/index.ts`);
+    if (args.endpoint) {
+      await downloadSchema(args.endpoint, flags.out || "schema.graphql");
+      this.log(`Successfully download your GraphQL schema!`);
+    } else {
+      this.warn("Invalid ENDPOINT");
+      this._help();
+    }
   }
 }
 
